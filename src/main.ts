1ad34a240219088a5a1b5e5714911cad0c48dc92
Adam Tran <adam@tdtran.cz>
2026-04-16 16:12:22 +0200

    send telegram msg of all offers

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,26 @@
-// Crawlee - web scraping and browser automation library (Read more at https://crawlee.dev)
 import { CheerioCrawler } from '@crawlee/cheerio';
-// Apify SDK - toolkit for building Apify Actors (Read more at https://docs.apify.com/sdk/js/)
 import { Actor } from 'apify';
 
-// this is ESM project, and as such, it requires you to specify extensions in your relative imports
-// read more about this here: https://nodejs.org/docs/latest-v18.x/api/esm.html#mandatory-file-extensions
-// note that we need to use `.js` even when inside TS files
 import { router } from './routes.js';
 
-interface Input {
-    startUrls: {
-        url: string;
-        method?: 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'TRACE' | 'OPTIONS' | 'CONNECT' | 'PATCH';
-        headers?: Record<string, string>;
-        userData: Record<string, unknown>;
-    }[];
-    maxRequestsPerCrawl: number;
-}
-
-// The init() call configures the Actor to correctly work with the Apify-provided environment - mainly the storage infrastructure. It is necessary that every Actor performs an init() call.
 await Actor.init();
 
-// Structure of input is defined in input_schema.json
-const { startUrls = ['https://apify.com'], maxRequestsPerCrawl = 100 } =
-    (await Actor.getInput<Input>()) ?? ({} as Input);
+interface Input {
+    urls: { url: string }[];
+    startDate?: string;
+    telegramToken?: string;
+    telegramChatId?: string;
+}
 
-// `checkAccess` flag ensures the proxy credentials are valid, but the check can take a few hundred milliseconds.
-// Disable it for short runs if you are sure your proxy configuration is correct
-const proxyConfiguration = await Actor.createProxyConfiguration({ checkAccess: true });
+const input = await Actor.getInput<Input>();
+if (!input) throw new Error('Input is required');
+const { urls, telegramToken, startDate, telegramChatId } = input;
 
 const crawler = new CheerioCrawler({
-    proxyConfiguration,
-    maxRequestsPerCrawl,
+    maxRequestsPerCrawl: 100,
     requestHandler: router,
 });
 
-await crawler.run(startUrls);
+await crawler.run(urls.map((u) => ({ ...u, userData: { startDate, telegramToken, telegramChatId } })));
 
-// Gracefully exit the Actor process. It's recommended to quit all Actors with an exit()
 await Actor.exit();
--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,45 @@ import { createCheerioRouter } from '@crawlee/cheerio';
 
 export const router = createCheerioRouter();
 
-router.addDefaultHandler(async ({ enqueueLinks, request, $, log, pushData }) => {
-    log.info('enqueueing new URLs');
-    await enqueueLinks();
+router.addDefaultHandler(async ({ enqueueLinks, request, $, log }) => {
+    const offerLinks: string[] = [];
+    const startDate = new Date(request.userData.startDate);
+    $('a.cb-offer[href*="/inzerat/"]').each((i, el) => {
+        const publishedString = $(el).find('.cb-time-ago').attr('title');
+        const publishedAt = /\d+\. \d+\. \d{4}/.exec(publishedString ?? '')?.[0];
+        if (publishedAt) {
+            const [day, month, year] = publishedAt.split('.').map(Number);
+            const publishedDate = new Date(year, month - 1, day);
+            if (publishedDate < startDate) return;
+        }
+        const offerLink = $(el).attr('href');
+        if (offerLink) {
+            log.info(`Found offer ${i}: ${offerLink}`);
+            offerLinks.push(offerLink);
+        }
+    });
+    await enqueueLinks({ urls: offerLinks, baseUrl: request.loadedUrl, label: 'OFFER' });
+});
 
-    // Extract title from the page.
-    const title = $('title').text();
-    log.info(`${title}`, { url: request.loadedUrl });
+router.addHandler('OFFER', async ({ request, $, log, pushData }) => {
+    const title = $('.offer-detail__header h1').text().trim();
+    const price = $('.cb-seller-box__price').text().trim();
+    const description = $('.offer-detail__desc').text().trim();
+    const created = $('.cb-time-ago').attr('title');
+    const location = $('.cb-seller-box__location').text().trim();
+    await pushData({ title, price, description, created, location, url: request.loadedUrl });
 
-    // Save url and title to Dataset - a table-like storage.
-    await pushData({ url: request.loadedUrl, title });
+    try {
+        const { telegramToken, telegramChatId } = request.userData;
+        if (telegramToken && telegramChatId) {
+            const message = `${title}\nPrice: ${price}\nLocation: ${location}\nCreated: ${created}\nURL: ${request.loadedUrl}`;
+            await fetch(`https://api.telegram.org/bot${telegramToken}/sendMessage`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ chat_id: telegramChatId, text: message }),
+            });
+        }
+    } catch {
+        log.error('Error sending Telegram message.');
+    }
 });
